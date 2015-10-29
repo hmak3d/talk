1e3ba5740327f22e4dd48981eec9ece2d86d2312
Howard Mak <howard@grabcad.com>
2015-10-28 23:00:47 -0400

    Basic React component

--- pub/main.jsx
+++ pub/main.jsx
@@ -4,4 +4,16 @@ const React = require('react');
 const ReactDOM = require('react/lib/ReactDOM');
 const Bootstrap = require('react-bootstrap');
 
-ReactDOM.render((<div>hello world</div>), document.getElementById('reactMount'));
+let Wrapper = React.createClass({
+    render: function() {
+        return (<div className="wrapper">{this.props.children}</div>);
+    }
+});
+
+let MyComponent = React.createClass({
+    render: function() {
+        return (<Wrapper>hello world</Wrapper>);
+    }
+});
+
+ReactDOM.render((<MyComponent/>), document.getElementById('reactMount'));
