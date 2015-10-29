82fe8bfda75258524d3a786ee6ae2995bae0f0bc
Howard Mak <howard@grabcad.com>
2015-10-28 22:56:49 -0400

    Create basic React app using node modules running in vanilla browser via Browserify

---
+++ pub/main.jsx
@@ -0,0 +1,7 @@
+'use strict';
+
+const React = require('react');
+const ReactDOM = require('react/lib/ReactDOM');
+const Bootstrap = require('react-bootstrap');
+
+ReactDOM.render((<div>hello world</div>), document.getElementById('reactMount'));
